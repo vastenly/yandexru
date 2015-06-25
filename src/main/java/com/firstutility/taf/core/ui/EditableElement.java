5d229b2ef6b631fc7ac4c08e95aa0738fbf73d0f
Alexander Rabetski <a.rabetski@godeltech.com>
2015-06-25 17:26:24 +0300

    -editable element waits updated / required for story 224

--- src/main/java/com/firstutility/taf/core/ui/EditableElement.java
+++ src/main/java/com/firstutility/taf/core/ui/EditableElement.java
@@ -14,51 +14,54 @@ public class EditableElement extends Element {
 		super(locator, name);
 	}
 
-	public boolean isEditable() {
+	public boolean isEnabled() {
 		try {
-			if (driver.findElement(lh.getByType(locator)).isEnabled()) {
+			if (driver.findElement(lh.getByType(locator)).isEnabled())
 				return true;
-			}
 	    } catch (NoSuchElementException e) {
 	        return false;
 	    }
 	    return false;
 	}
 	
-	public boolean isEnabled() {
-		if (isVisible() && isEditable()) {
+	public boolean isEditable() {
+		if (isVisible() && isEditable())
 			return true;
+		return false;
+	}
+	
+	public boolean waitForToBeEnabled() {
+		int timeout = 10;
+		return waitForToBeEnabled(timeout);
+	}
+	
+	public boolean waitForToBeEnabled(int timeout) {
+		for (int second = 0; ; second++) {
+			if (second >= timeout)
+				break;
+			try {
+				if (isEnabled())
+					return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			WaitUtils.timeout(1, TimeUnit.SECONDS);
 		}
 		return false;
 	}
 	
 	public boolean waitForToBeEditable() {
 		int timeout = 10;
-		for (int second = 0; ; second++) {
-			if (second >= timeout) {
-				break;
-			}
-			try {
-				if (isEditable()) {
-					return true;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			WaitUtils.timeout(1, TimeUnit.SECONDS);
-		}	
-		return false;
+		return waitForToBeEditable(timeout);
 	}
 	
 	public boolean waitForToBeEditable(int timeout) {
 		for (int second = 0; ; second++) {
-			if (second >= timeout) {
+			if (second >= timeout)
 				break;
-			}
 			try {
-				if (isEditable()) {
+				if (isEditable())
 					return true;
-				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -66,17 +69,19 @@ public class EditableElement extends Element {
 		}
 		return false;
 	}
-
-	public boolean waitForToBeNotEditable() {
+	
+	public boolean waitForToBeNotEnabled() {
 		int timeout = 10;
+		return waitForToBeNotEnabled(timeout);
+	}
+
+	public boolean waitForToBeNotEnabled(int timeout) {
 		for (int second = 0; ; second++) {
-			if (second >= timeout) {
+			if (second >= timeout)
 				break;
-			}
 			try {
-				if (!isVisible()) {
+				if (!isEnabled())
 					return true;
-				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -85,15 +90,18 @@ public class EditableElement extends Element {
 		return false;
 	}
 	
+	public boolean waitForToBeNotEditable() {
+		int timeout = 10;
+		return waitForToBeNotEditable(timeout);
+	}
+	
 	public boolean waitForToBeNotEditable(int timeout) {
 		for (int second = 0; ; second++) {
-			if (second >= timeout) {
+			if (second >= timeout)
 				break;
-			}
 			try {
-				if (!isVisible()) {
+				if (!isEditable())
 					return true;
-				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
